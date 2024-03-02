12593f825b737ba95563a31c01fe29736c2a3cb6
Eugenio <68464938+EugenioUglov@users.noreply.github.com>
2024-03-02 19:28:03 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const http = require('http');
+const express = require('express');
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
+var router = express.Router();
+
+router.get('/test/:id', function (req, res) { 
+    res.render('test', {output: req.params.id});
+});
+
+
 const server = http.createServer((req, res) => { 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Test done');
+    res.end('Test done 2');
 });
 
 server.listen(port, hostname, () => { 
     console.log('test');
 });
 
-let pluralizer = require('./pluralize.js');
+
+
+
+
+let pluralizer = require("./pluralize.js");
 console.log(pluralizer.singular("cars"));
 console.log(pluralizer.plural("family"));
