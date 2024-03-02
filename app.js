a6adfbf078acb2dbffc022dce1a0d6109b470e2c
Eugenio <eugeniouglov@gmail.com>
2024-03-02 20:46:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const url = require("url");
-const express = require("express");
+// const express = require("express");
 
 const hostname = "0.0.0.0";
 const port = 3000;
@@ -10,22 +10,22 @@ const port = 3000;
 // });
 
 const server = http.createServer((req, res) => {
-  let urlParts = url.parse(req.url);
+  // let urlParts = url.parse(req.url);
 
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  //   res.end("Test done 2");
+    res.end("Test done 2");
 
-  if (req.method === "GET") {
-    switch (urlParts.pathname) {
-      case "/test:id":
-        res.end("Test");
-        break;
+  // if (req.method === "GET") {
+  //   switch (urlParts.pathname) {
+  //     case "/test:id":
+  //       res.end("Test");
+  //       break;
 
-      default:
-        break;
-    }
-  }
+  //     default:
+  //       break;
+  //   }
+  // }
 });
 
 server.listen(port, hostname, () => {
