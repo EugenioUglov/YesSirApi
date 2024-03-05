ab1e2635933a75f91c7c4de5d8296028ec474768
Eugenio <eugeniouglov@gmail.com>
2024-03-05 12:44:39 +0100

    Access-Control-Allow-Methods

--- app.js
+++ app.js
@@ -6,9 +6,28 @@ const app = express();
 const hostname = "0.0.0.0";
 const port = 3000;
 
-
 let pluralizer = require("./pluralize.js");
-  
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://eugeniouglov.github.io"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.get("/singular", function (req, res) {
   const requestParam = req.query.request;
@@ -33,9 +52,6 @@ app.get("/plural", function (req, res) {
 //   res.setHeader("Content-Type", "text/plain");
 //   res.end("Test done 2");
 // });
-
-
-
 
 app.listen(3000, () => {
   console.log("sercver listen");
